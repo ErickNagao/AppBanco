33ce70292e4e8328331b63d6f96bb75956e50681
Erick Nagao <ericknagao03@gmail.com>
2025-12-01 15:01:58 -0300

    fix(util): restrict CsvExporter to valid transaction types only

--- src/util/CsvExporter.java
+++ src/util/CsvExporter.java
@@ -11,7 +11,9 @@ public class CsvExporter {
         try (FileWriter fw = new FileWriter(path)) {
             fw.write("Data/Hora,Tipo,Valor,Conta Origem,Conta Destino,Saldo Após,Descrição\n");
             for (Transaction t : transactions) {
-                if (t.getType() != null && t.getType().equalsIgnoreCase("CREATE")) continue;
+                 if (t.getType() == null) continue;
+                 String type = t.getType().toUpperCase();
+                 if (!(type.equals("DEPOSIT") || type.equals("WITHDRAW") || type.equals("TRANSFER") || type.equals("TRANSFER_IN"))) continue;
                 fw.write(t.toCsvLine() + "\n");
             }
         }
