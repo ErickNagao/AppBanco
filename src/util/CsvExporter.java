0056fe9a28e2dccd5b861a9f2f22f6712f633100
Erick Nagao <ericknagao03@gmail.com>
2025-12-01 14:30:24 -0300

    feat(csv): update header in exportTransactions

--- src/util/CsvExporter.java
+++ src/util/CsvExporter.java
@@ -9,7 +9,7 @@ import model.Transaction;
 public class CsvExporter {
     public static void exportTransactions(List<Transaction> transactions, String path) throws IOException {
         try (FileWriter fw = new FileWriter(path)) {
-            fw.write("timestamp,type,amount,fromAccount,toAccount,balanceAfter,description\n");
+            fw.write("Data/Hora,Tipo,Valor,Conta Origem,Conta Destino,Saldo Após,Descrição\n");
             for (Transaction t : transactions) {
                 if (t.getType() != null && t.getType().equalsIgnoreCase("CREATE")) continue;
                 fw.write(t.toCsvLine() + "\n");
